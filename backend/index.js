e64f4f2d54262818b0fe0250032cd3af694b1fd9
vijaykrishna483-cms <vijay762005@gmail..com>
2025-05-05 21:56:06 +0530

    DFDF

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,9 @@ const PORT = process.env.PORT || 8000;
 
 
 
-
-app.use(cors("*"));
+app.use(cors({
+    origin: ['https://trackmyfundss.netlify.app', 'http://localhost:5173'], 
+  }));
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true }));
 
